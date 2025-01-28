a513564d89b58e197207e3b74b170d3af491df86
SaucyB03 <105938116+SaucyB03@users.noreply.github.com>
2025-01-28 08:08:21 -0500

    added joints

---
+++ generate.py
@@ -0,0 +1,22 @@
+import pyrosim.pyrosim as pyrosim
+
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-2.0,2.0, 0.5] , size=[1,1,1])
+    pyrosim.End()   
+
+    
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0, 1.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Back_Torso" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [-0.5,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Front_Torso" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[1,1,1])
+    pyrosim.End()   
+
+
+Create_World()
+Create_Robot()
